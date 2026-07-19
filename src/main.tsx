ee3e221cf95fc0b657d357d56d2ab9ff836f9051
KassiusPrime <cassianokaique9@gmail.com>
2026-07-19 16:36:14 -0300

    Atualizar o main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { registerSW } from 'virtual:pwa-register';
+
+// Isso garante que o app se atualize sozinho sempre que houver novidade no GitHub
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
